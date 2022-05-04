419aaa4811651308813bb749734142310e1e3a47
NJSal <salazar.davidf@gmail.com>
2022-05-03 23:47:33 -0400

    commit: added 1 for third parameter

--- tests/user_test.py
+++ tests/user_test.py
@@ -11,7 +11,7 @@ def test_adding_user(application):
         assert db.session.query(Song).count() == 0
         #showing how to add a record
         #create a record
-        user = User('keith@webizly.com', 'testtest')
+        user = User('keith@webizly.com', 'testtest', 1)
         #add it to get ready to be committed
         db.session.add(user)
         #call the commit
